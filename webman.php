57dda9c1871c928b74b924f773ece5803f1a72f2
lyqray <62620282+lyqray@users.noreply.github.com>
2024-09-13 04:26:48 +0800

    Update webman.php

--- webman.php
+++ webman.php
@@ -12,7 +12,7 @@ Adapterman::init();
 
 $ncpu = substr_count((string)@file_get_contents('/proc/cpuinfo'), "\nprocessor")+1;
 
-$http_worker                = new Worker('http://127.0.0.1:6600');
+$http_worker                = new Worker('http://127.0.0.1:6610');
 $http_worker->count         = $ncpu * 2;
 $http_worker->name          = 'AdapterMan';
 
