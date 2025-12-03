e92e7ad070e61855be5e858bccb27a04cfbe10bd
lyqray <62620282+lyqray@users.noreply.github.com>
2025-12-04 07:58:57 +0800

    clash系修復vless-encryption無tls下發flow流控

--- app/Protocols/ClashNyanpasu.php
+++ app/Protocols/ClashNyanpasu.php
@@ -284,7 +284,7 @@ class ClashNyanpasu
 
         if (isset($server['encryption']) && !empty($server['encryption']) && isset($server['encryption_settings']) && !empty($server['encryption_settings'])) {
             $encryptionSettings = $server['encryption_settings'];
-			$array['flow'] = !empty($server['flow']) ? $server['flow']: "";
+            $array['flow'] = !empty($server['flow']) ? $server['flow']: "";
             $array['encryption'] = $server['encryption'] ?? 'mlkem768x25519plus';
             $array['encryption'] .= '.' . $encryptionSettings['mode'] ?? 'native';
             $array['encryption'] .= '.' . $encryptionSettings['rtt'] ?? '1rtt';
